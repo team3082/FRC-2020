42d19d5de55995bf2a0958bb94bdf31598c2e8c9
JackO-Personal <jackokane5428@gmail.com>
2020-02-04 16:32:41 -0600

    lllll

---
+++ src/main/java/frc/robot/JoystickButtonToggle.java
@@ -0,0 +1,46 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+public class JoystickButtonToggle extends JoystickButton {
+
+    /**
+     * The value of whether or not the button is toggled "on" or "off"
+     */
+    private boolean buttonToggle = false;
+    /**
+     * The value of the button the last time get() or getToggle() was called
+     */
+    private boolean buttonLast = false;
+
+    /**
+     * Create a joystick button for triggering commands.
+     *
+     * @param joystick     The GenericHID object that has the button (e.g. Joystick, KinectStick,
+     *                     etc)
+     * @param buttonNumber The button number (see {@link GenericHID#getRawButton(int) }
+     */
+    public JoystickButtonToggle(GenericHID joystick, int buttonNumber) {
+        super(joystick, buttonNumber);
+    }
+
+    public boolean get() {
+        boolean value = super.get();
+        update(value);
+        return value;
+    }
+
+    public boolean getToggle() {
+        update(super.get());
+        return buttonToggle;
+    }
+
+    private void update(boolean buttonValue) {
+        if(buttonValue && !buttonLast) {
+            buttonToggle = !buttonToggle;
+        }
+        buttonLast = buttonValue;
+    }
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,9 +7,18 @@
 
 package frc.robot;
 
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,87 +27,102 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot
-{
-    private Command autonomousCommand;
+public class Robot extends TimedRobot {
+    private static final String DEFAULT_AUTO = "Default";
+    private static final String CUSTOM_AUTO = "My Auto";
+    private String autoSelected;
+    private final SendableChooser<String> chooser = new SendableChooser<>();
 
-    private RobotContainer robotContainer;
+    boolean buttonToggle = false;
+    boolean driveButtonLast = false;
+    boolean testButtonToggleFun = false;
+    
+    int s = 0;
+
+    // Motors
+    private WPI_TalonSRX motorLeft0 = new WPI_TalonSRX(Constants.MOTOR_LEFTFRONT);
+    private WPI_TalonSRX motorLeft1 = new WPI_TalonSRX(Constants.MOTOR_LEFTBACK);
+    private WPI_TalonSRX motorRight2 = new WPI_TalonSRX(Constants.MOTOR_RIGHTFRONT);
+    private WPI_TalonSRX motorRight3 = new WPI_TalonSRX(Constants.MOTOR_RIGHTBACK);
+    private Talon flyWheel1 = new Talon(Constants.FLYWHEEL_MOTOR1);
+    private Talon flyWheel2 = new Talon(Constants.FLYWHEEL_MOTOR2);
+
+    //Motor Control Groups
+    private SpeedControllerGroup motorsLeft = new SpeedControllerGroup(motorLeft0, motorLeft1);
+    private SpeedControllerGroup motorsRight = new SpeedControllerGroup(motorRight2, motorRight3);
+
+    //Drivetrain object
+    private final DifferentialDrive drive = new DifferentialDrive(motorsLeft, motorsRight);
+
+    //Controller object
+    private final Joystick driveCntrl = new Joystick(0);
+
+    private final JoystickButton shootButton = new JoystickButton(driveCntrl, 1);
+    private final JoystickButton slowButton = new JoystickButton(driveCntrl, 2);
 
     /**
-     * This method is run when the robot is first started up and should be used for any
-     * initialization code.
+     * This method is run when the robot is first started up and should be
+     * used for any initialization code.
      */
     @Override
-    public void robotInit()
-    {
-        // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-        // autonomous chooser on the dashboard.
-        robotContainer = new RobotContainer();
+    public void robotInit() {
+        chooser.setDefaultOption("Default Auto", DEFAULT_AUTO);
+        chooser.addOption("My Auto", CUSTOM_AUTO);
+        SmartDashboard.putData("Auto choices", chooser);
+        
+        // testMotor.set(ControlMode.PercentOutput, 0);
+        motorLeft0.configFactoryDefault();
+        motorLeft1.configFactoryDefault();
+        motorRight2.configFactoryDefault();
+        motorRight3.configFactoryDefault();
+        // drive.setRightSideInverted(false);
     }
-
     /**
-     * This method is called every robot packet, no matter the mode. Use this for items like
-     * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-     *
-     * <p>This runs after the mode specific periodic functions, but before
+     * This method is called every robot packet, no matter the mode. Use
+     * this for items like diagnostics that you want ran during disabled,
+     * autonomous, teleoperated and test.
+     * <p>
+     * This runs after the mode specific periodic methods, but before
      * LiveWindow and SmartDashboard integrated updating.
      */
     @Override
     public void robotPeriodic()
     {
-        // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-        // commands, running already-scheduled commands, removing finished or interrupted commands,
-        // and running subsystem periodic() methods.  This must be called from the robot's periodic
-        // block in order for anything in the Command-based framework to work.
-        CommandScheduler.getInstance().run();
     }
 
     /**
-     * This method is called once each time the robot enters Disabled mode.
+     * This autonomous (along with the chooser code above) shows how to select
+     * between different autonomous modes using the dashboard. The sendable
+     * chooser code works with the Java SmartDashboard. If you prefer the
+     * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+     * getString line to get the auto name from the text box below the Gyro
+     * <p>
+     * You can add additional auto modes by adding additional comparisons to
+     * the switch structure below with additional strings. If using the
+     * SendableChooser make sure to add them to the chooser code above as well.
      */
     @Override
-    public void disabledInit()
-    {
-    }
-
-    @Override
-    public void disabledPeriodic()
-    {
-    }
-
-    /**
-     * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
-     */
-    @Override
-    public void autonomousInit()
-    {
-        autonomousCommand = robotContainer.getAutonomousCommand();
-
-        // schedule the autonomous command (example)
-        if (autonomousCommand != null)
-        {
-            autonomousCommand.schedule();
-        }
+    public void autonomousInit() {
+        autoSelected = chooser.getSelected();
+        // autoSelected = SmartDashboard.getString("Auto Selector",
+        // defaultAuto);
+        System.out.println("Auto selected: " + autoSelected);
     }
 
     /**
      * This method is called periodically during autonomous.
      */
     @Override
-    public void autonomousPeriodic()
-    {
-    }
-
-    @Override
-    public void teleopInit()
-    {
-        // This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        if (autonomousCommand != null)
+    public void autonomousPeriodic() {
+        switch (autoSelected)
         {
-            autonomousCommand.cancel();
+            case CUSTOM_AUTO:
+                // Put custom auto code here
+                break;
+            case DEFAULT_AUTO:
+            default:
+                // Put default auto code here
+                break;
         }
     }
 
@@ -106,16 +130,85 @@ public class Robot extends TimedRobot
      * This method is called periodically during operator control.
      */
     @Override
-    public void teleopPeriodic()
-    {
+    public void teleopPeriodic() {
+        // double speedmod; 
+        // if(slowButton.get()){
+        //     speedmod = 0.4;
+        // }
+        // else {
+        //     speedmod = 0.8;
+        // }
+        
+        double x = driveToggle(slowButton.get());
+        
+
+        // drive.arcadeDrive(-driveCntrl.getY() * x, driveCntrl.getX() * x);
+
+        flyWheel1.setSpeed(driveCntrl.getRawAxis(3));
+        flyWheel2.setSpeed(-driveCntrl.getRawAxis(3));
+
+        
+        
+        
+
+
+        
+        if(shootButton.get() && testButtonToggleFun == false) {
+            s = (s + 1) % 4; 
+        }
+
+        testButtonToggleFun = shootButton.get();
+
+        switch(s){
+            case 0:
+                drive.arcadeDrive(0, 0);
+                break;
+            case 1:
+                drive.arcadeDrive(0.1, 0);
+                break;
+            case 2:
+                drive.arcadeDrive(0, 0);
+                break;
+            case 3:
+                drive.arcadeDrive(-0.1, 0);
+
+            
+        }
+        
+
+        // if(shootButton.get() == true) {
+        //     // flyWheel.setSpeed(0.6);
+        //     flyWheel1.setSpeed(1);
+        //     flyWheel2.setSpeed(-1);
+
+        //     SmartDashboard.putBoolean("flyon", true);
+            
+        // }
+        // else{
+        //     flyWheel1.setSpeed(0);
+        //     flyWheel2.setSpeed(0);
+        //     SmartDashboard.putBoolean("flyon", false);
+        // }
+    } 
+
+    public double driveToggle(boolean buttonValue) {
+        double speedmod;   
+
+        if(buttonValue && !driveButtonLast)
+            buttonToggle = !buttonToggle;     
+
+            driveButtonLast = buttonValue;
+
+        if(buttonToggle)
+            speedmod = 0.4;
+
+        else
+            speedmod = 0.8;
+
+        return speedmod;
     }
 
-    @Override
-    public void testInit()
-    {
-        // Cancels all running commands at the start of test mode.
-        CommandScheduler.getInstance().cancelAll();
-    }
+
 
     /**
      * This method is called periodically during test mode.
